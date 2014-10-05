f10f015634d73e87a365ff19e442a16b0adb1494
Alaf Azam Khan <alafazam@gmail.com>
2014-10-05 22:35:56 +0530

    lo kar diya

--- application/views/academic_view.php
+++ application/views/academic_view.php
@@ -1,3 +1,10 @@
+<?php
+$data= $this->session->userdata('logged_in');
+$username=$data['username'];
+$flashSuccess =$this->session->flashdata('flashSuccess');
+$fname=$data['fname'];
+?>
+
 <form class="form-horizontal">
 <fieldset>
 <!-- Form Name -->
@@ -7,114 +14,98 @@
   <label class="col-md-4 control-label" for="cursem">Current Semester</label>
   <div class="col-md-4">
     <select id="cursem" name="cursem" class="form-control">
-      <option value="1">I</option>
-      <option value="2">II</option>
-      <option value="3">III</option>
-      <option value="4">IV</option>
-      <option value="5">V</option>
-      <option value="6">VI</option>
-      <option value="7">VII</option>
-      <option value="8">VIII</option>
+      <option <?php if ($cursem==='1') {echo 'selected'}?> value="1">I</option>
+      <option <?php if ($cursem==='2') {echo 'selected'}?> value="2">II</option>
+      <option <?php if ($cursem==='3') {echo 'selected'}?> value="3">III</option>
+      <option <?php if ($cursem==='4') {echo 'selected'}?> value="4">IV</option>
+      <option <?php if ($cursem==='5') {echo 'selected'}?> value="5">V</option>
+      <option <?php if ($cursem==='6') {echo 'selected'}?> value="6">VI</option>
+      <option <?php if ($cursem==='7') {echo 'selected'}?> value="7">VII</option>
+      <option <?php if ($cursem==='8') {echo 'selected'}?> value="8">VIII</option>
     </select>
   </div>
 </div>
 <!-- Text input-->
-<div class="form-group">
+<div class="form-group row">
   <label class="col-md-4 control-label" for="sem1">Semester I</label>  
-  <div class="col-md-4">
-  <input id="sem1" name="sem1" type="text" placeholder="SGPA I" class="form-control input-md">
-    
+  <div class="col-md-2">
+  <input id="sem1" name="sem1" value="<?php if ($sem1) {echo $sem1}?>" type="text" placeholder="SGPA I" class="form-control input-md">
+  </div>
+  <label class="col-md-2 control-label" for="sem2">Semester II</label>  
+  <div class="col-md-2">
+  <input id="sem2" name="sem2" value="<?php if ($sem2) {echo $sem2}?>" type="text" placeholder="SGPA II" class="form-control input-md">    
   </div>
 </div>
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sem2">Semester II</label>  
-  <div class="col-md-4">
-  <input id="sem2" name="sem2" type="text" placeholder="SGPA II" class="form-control input-md">
-    
-  </div>
-</div>
+
+
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="sem3">Semester III</label>  
-  <div class="col-md-4">
-  <input id="sem3" name="sem3" type="text" placeholder="SGPA III" class="form-control input-md">
-    
+  <div class="col-md-2">
+  <input id="sem3" name="sem3" value="<?php if ($sem3) {echo $sem3}?>" type="text" placeholder="SGPA III" class="form-control input-md">
   </div>
-</div>
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sem4">Semester IV</label>  
-  <div class="col-md-4">
-  <input id="sem4" name="sem4" type="text" placeholder="SGPA IV" class="form-control input-md">
-    
+  <label class="col-md-2 control-label" for="sem4">Semester IV</label>  
+  <div class="col-md-2">
+  <input id="sem4" name="sem4" value="<?php if ($sem4) {echo $sem4}?>" type="text" placeholder="SGPA IV" class="form-control input-md">
   </div>
 </div>
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="sem5">Semester V</label>  
-  <div class="col-md-4">
-  <input id="sem5" name="sem5" type="text" placeholder="SGPA V" class="form-control input-md">
-    
+  <div class="col-md-2">
+  <input id="sem5" name="sem5" value="<?php if ($sem5) {echo $sem5}?>" type="text" placeholder="SGPA V" class="form-control input-md">
+  </div>
+  <label class="col-md-2 control-label" for="sem6">Semester VI</label>  
+  <div class="col-md-2">
+  <input id="sem6" name="sem6" value="<?php if ($sem6) {echo $sem6}?>" type="text" placeholder="SGPA VI" class="form-control input-md"> 
   </div>
 </div>
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sem6">Semester VI</label>  
-  <div class="col-md-4">
-  <input id="sem6" name="sem6" type="text" placeholder="SGPA VI" class="form-control input-md">
-    
-  </div>
-</div>
+
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="sem7">Semester VII</label>  
-  <div class="col-md-4">
-  <input id="sem7" name="sem7" type="text" placeholder="SGPA VII" class="form-control input-md">
-    
+  <div class="col-md-2">
+  <input id="sem7" name="sem7" value="<?php if ($sem7) {echo $sem7}?>" type="text" placeholder="SGPA VII" class="form-control input-md">
+  </div>
+  <label class="col-md-2 control-label" for="sem8">Semester VIII</label>  
+  <div class="col-md-2">
+  <input id="sem8" name="sem8" value="<?php if ($sem8) {echo $sem8}?>" type="text" placeholder="SGPA VIII" class="form-control input-md"> 
   </div>
 </div>
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sem8">Semester VIII</label>  
-  <div class="col-md-4">
-  <input id="sem8" name="sem8" type="text" placeholder="SGPA VIII" class="form-control input-md">
-    
-  </div>
-</div>
+
+<legend></legend>
+
 <!-- Select Basic -->
 <div class="form-group">
   <label class="col-md-4 control-label" for="supply">Supplementary</label>
-  <div class="col-md-4">
+  <div class="col-md-2">
     <select id="supply" name="supply" class="form-control">
-      <option value="1">Yes</option>
-      <option value="2">No</option>
+      <option <?php if ($supply==='1') {echo 'selected'}?> value="1">Yes</option>
+      <option <?php if ($supply==='2') {echo 'selected'}?> value="2">No</option>
     </select>
   </div>
-</div>
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="back">Backlog</label>
-  <div class="col-md-4">
+  <label class="col-md-2 control-label" for="back">Backlog</label>
+  <div class="col-md-2">
     <select id="back" name="back" class="form-control">
-      <option value="1">Yes</option>
-      <option value="2">No</option>
+      <option <?php if ($back==='1') {echo 'selected'}?> value="1">Yes</option>
+      <option <?php if ($back==='2') {echo 'selected'}?> value="2">No</option>
     </select>
   </div>
 </div>
+
+
 <!-- Textarea -->
 <div class="form-group">
   <label class="col-md-4 control-label" for="reason">Reason for Supplementary/ Backlog If Any</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="reason" name="reason"></textarea>
+    <textarea class="form-control" id="reason" name="reason"><?php if ($reason) {echo $reason}?></textarea>
   </div>
 </div>
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="cgpa">Overall CGPA</label>  
   <div class="col-md-4">
-  <input id="cgpa" name="cgpa" type="text" placeholder="CGPA" class="form-control input-md">
-    
+  <input id="cgpa" name="cgpa" value="<?php if (cgpa) {echo 'selected'}?>" type="text" placeholder="CGPA" class="form-control input-md">
   </div>
 </div>
 <!-- Button -->
--- testdb struckture.sql
+++ testdb struckture.sql
@@ -1,5 +1,5 @@
 CREATE TABLE StudentDetails (
-    id       INT           NOT NULL AUTO_INCREMENT,
+    id               INT           NOT NULL AUTO_INCREMENT,
     username         VARCHAR (50)  NOT NULL,
     password         VARCHAR (100) NOT NULL,
     enroll           INT           NULL,
@@ -24,7 +24,7 @@ CREATE TABLE StudentDetails (
     Technical_Skills TEXT          NULL,
     Other_skills     TEXT          NULL,
     Project_id       INT           NULL,
-    PRIMARY KEY CLUSTERED (student_id ASC)
+    PRIMARY KEY CLUSTERED (id ASC)
 );
 
 
@@ -32,14 +32,14 @@ CREATE TABLE StudentDetails (
 CREATE TABLE school_details
 (
     school_id               INT                NOT NULL AUTO_INCREMENT, 
-    student_id              INT                NOT NULL  , 
+    id              INT                NOT NULL  , 
     type                    INT                NOT NULL, 
     school_name             VARCHAR(80)        NULL, 
     board                   VARCHAR(50)        NULL, 
     year                    NCHAR(4)           NULL, 
     percentage              NCHAR(9)           NULL, 
     PRIMARY KEY (school_id), 
-    CONSTRAINT FK_Table_ToTable FOREIGN KEY (student_id) REFERENCES StudentDetails(student_id)
+    CONSTRAINT FK_Table_ToTable FOREIGN KEY (id) REFERENCES StudentDetails(id)
 );
 
 //for type 1 we have 12th details
@@ -49,17 +49,17 @@ CREATE TABLE school_details
 CREATE TABLE applications
 (
     application_id INT      NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    student_id INT          NOT NULL, 
+    id INT          NOT NULL, 
     Company VARCHAR(50)     NULL, 
     Status VARCHAR(50)      NULL, 
     date VARCHAR(50)        NULL,
     cover_letter TEXT       NULL, 
-    CONSTRAINT FK_Applications_ToTable FOREIGN KEY (student_id) REFERENCES StudentDetails(student_id)
+    CONSTRAINT FK_Applications_ToTable FOREIGN KEY (id) REFERENCES StudentDetails(id)
 );
 
 
 INSERT INTO StudentDetails (enroll, branch,year_of_admin, FNAME, MNAME, LNAME, 12_std_id, 10_std_id, AIEEE_id, score_sheet_id,Parents_name, AddressL1, AddressL2, City, Email_add, Phone, ECA, Career_obj, Technical_Skills, Other_skills, Project_id) VALUES (552, N'IT', NULL, N'Alaf', N'Azam', N'Khan', NULL, NULL, NULL, NULL, NULL, N'Iqbal Ahmad', N'34 ashoka colony', N'MAnikh bagh road', N'indore', N'alafazam@gmail.com', N'0898989898', N'sdkjvbsdjkvnskjskvnskvns dkncvsdkjvns kdvns vnsdjkn', N'akvnsdkjvnskdj  adskn vskdjvnskj v skjds knvvdskjv dkv sjdv', N'akj fnkajfnaksj sdfns kdjfnskdjnf skdjn  askjfnskjfnsdkj  akjfnakjfnakjfn  akjfdnaksjfn ', N' kajssfnkajfnakjfn  akjfnakfnasjkfn  askjkjfnakj akfn', NULL);
 
-INSERT INTO school (student_id, school_name, city, year, board, paercentage, type) VALUES (1, N'asdas', N'asfasdf', N'4566', N'sdfsdfsd', N'fsddfsdf  ', 1)
+INSERT INTO school (id, school_name, city, year, board, paercentage, type) VALUES (1, N'asdas', N'asfasdf', N'4566', N'sdfsdfsd', N'fsddfsdf  ', 1)
 
 INSERT INTO StudentDetails (username, password) values ('alafazam', MD5('alafazam'));
